a41a347f16b884b6910b5acd34c5f88b71089845
Bill Beebe <wbeebe@users.noreply.github.com>
2023-01-27 20:53:47 -0500

    Add Apache licensing everywhere.

--- usb-device-monitor/devices.cpp
+++ usb-device-monitor/devices.cpp
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2023 William H. Beebe, Jr.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include <algorithm>
 #include <iostream>
 #include <filesystem>
@@ -24,6 +40,21 @@ void split(const string &subj, const regex &rgx, array<string, 2> &vars) {
     vars[1] = match.str(0).erase(0,1);
     vars[1].erase(vars[1].end()-1);
 }
+
+/* This originally started as a command-line utility, one of three in which
+ * the other two were written in Python and Rust. It was a test of language
+ * utility.
+ * 
+ * The function looks down /dev/serial/by-id to find any and all USB devices
+ * plugged in. It turns out that the entries down that path are links to
+ * equivalent devices in /dev. The function reads them all, follows the links
+ * to get the device names, then organizes the information to return to the
+ * caller.
+ * 
+ * The data is returned as an array of device, interface name, and a unique
+ * hexadecimal ID. The idea behind this is that while the devices may shift,
+ * the unique hexadecimal IDs will not change.
+ */
 
 device_collection getDevices() {
     const string DEVICE_PATH{"/dev/serial/by-id/"};
